c5b23558242a04d18f5d4234218422e7370dc3d5
hongqingWang <272338444@qq.com>
2017-12-10 23:31:45 +0800

    pod 添加第三方库
    pod 添加第三方库

---
+++ QQCellHeight/QQCellHeight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQCellHeight
+//
+//  Created by 王红庆 on 10/12/2017.
+//  Copyright © 2017 王红庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQCellHeight/QQCellHeight/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QQCellHeight
+//
+//  Created by 王红庆 on 10/12/2017.
+//  Copyright © 2017 王红庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
